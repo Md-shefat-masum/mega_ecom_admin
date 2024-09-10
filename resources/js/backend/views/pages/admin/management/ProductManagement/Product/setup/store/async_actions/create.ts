0489e8925b304e7fc9426655021145a4b7e1f05f
Md-shefat-masum <mshefat924@gmail.com>
2024-09-11 01:03:19 +0600

    edit create service

--- resources/js/backend/views/pages/admin/management/ProductManagement/Product/setup/store/async_actions/create.ts
+++ resources/js/backend/views/pages/admin/management/ProductManagement/Product/setup/store/async_actions/create.ts
@@ -5,41 +5,6 @@ async function execute(event){
     let form = event.target;
     let form_data = new FormData(form);
 
-    let category_group = form.querySelector('[name="product_category_group_id"]');
-    let is_nav = form.querySelector('[name="is_nav"]');
-    let is_featured = form.querySelector('[name="is_featured"]');
-    let serial = form.querySelector('[name="serial"]');
-
-    form_data.delete('product_category_group_id');
-    form_data.delete('is_nav');
-    form_data.delete('is_featured');
-    form_data.delete('serial');
-
-    category_group = JSON.parse(category_group.value);
-    if(category_group.length){
-        form_data.append('product_category_group_id', category_group[0]);
-    }else{
-        form_data.append('product_category_group_id', "");
-    }
-
-    if(is_nav && is_nav.checked){
-        form_data.append('is_nav', "1");
-    }else{
-        form_data.append('is_nav', "0");
-    }
-
-    if(is_featured && is_featured.checked){
-        form_data.append('is_featured', "1");
-    }else{
-        form_data.append('is_featured', "0");
-    }
-
-    if(serial && serial.vlaue){
-        form_data.append('serial', serial.vlaue);
-    }else{
-        form_data.append('serial', '');
-    }
-
     let url = `${setup.api_host}/${setup.api_version}/${setup.api_end_point}/store`;
 
     try {
