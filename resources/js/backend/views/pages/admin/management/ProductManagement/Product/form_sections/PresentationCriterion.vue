2a8ac7c8679c4063b1e2ce521c7258b9f1d656f3
Md-shefat-masum <mshefat924@gmail.com>
2024-09-11 01:02:10 +0600

    inputs naming

--- resources/js/backend/views/pages/admin/management/ProductManagement/Product/form_sections/PresentationCriterion.vue
+++ resources/js/backend/views/pages/admin/management/ProductManagement/Product/form_sections/PresentationCriterion.vue
@@ -6,80 +6,72 @@
             </div>
             <div class="col-md-3">
                 <div class="pl-4">
-                    <input type="checkbox" class="form-check-input">
-                    <label class="ml-2">
+                    <input type="checkbox" name="is_available_in_e_commerce" id="is_available_in_e_commerce" class="form-check-input">
+                    <label class="ml-2" for="is_available_in_e_commerce">
                         Available in E-commerce
                     </label>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="pl-4">
-                    <input type="checkbox" class="form-check-input">
-                    <label class="ml-2">
+                    <input type="checkbox" name="is_feature_in_home_page" id="is_feature_in_home_page" class="form-check-input">
+                    <label class="ml-2" for="is_feature_in_home_page">
                         Feature on Home Page
                     </label>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="pl-4">
-                    <input type="checkbox" class="form-check-input">
-                    <label class="ml-2">
+                    <input type="checkbox" name="is_new_product" id="is_new_product" class="form-check-input">
+                    <label class="ml-2" for="is_new_product">
                         Mark as New
                     </label>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="pl-4">
-                    <input type="checkbox" class="form-check-input">
-                    <label class="ml-2">
+                    <input type="checkbox" name="is_trending" id="is_trending" class="form-check-input">
+                    <label class="ml-2" for="is_trending">
                         Mark as Trending
                     </label>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="pl-4">
-                    <input type="checkbox" class="form-check-input">
-                    <label class="ml-2">
+                    <input type="checkbox" name="is_best_selling" id="is_best_selling" class="form-check-input">
+                    <label class="ml-2" for="is_best_selling">
                         Mark as Best Selling
                     </label>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="pl-4">
-                    <input type="checkbox" class="form-check-input">
-                    <label class="ml-2">
-                        Mark as Best Selling
+                    <input type="checkbox" name="is_free_delivery" id="is_free_delivery" class="form-check-input">
+                    <label class="ml-2" for="is_free_delivery">
+                        Mark as Free Delivery
                     </label>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="pl-4">
-                    <input id="freee_delivery" type="checkbox" class="form-check-input">
-                    <label for="freee_delivery" class="ml-2">
-                        Free Delivery
+                    <input type="checkbox" name="is_upcomming" id="is_upcomming" class="form-check-input">
+                    <label class="ml-2" for="is_upcomming">
+                        Mark as Upcomming
                     </label>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="pl-4">
-                    <input id="freee_delivery" type="checkbox" class="form-check-input">
-                    <label for="freee_delivery" class="ml-2">
-                        Is Upcomming
-                    </label>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="pl-4">
-                    <input id="freee_delivery" type="checkbox" class="form-check-input">
-                    <label for="freee_delivery" class="ml-2">
+                    <input type="checkbox" name="is_emi_support" id="is_emi_support" class="form-check-input">
+                    <label class="ml-2" for="is_emi_support">
                         Is EMI Support
                     </label>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="pl-4">
-                    <input id="freee_delivery" type="checkbox" class="form-check-input">
-                    <label for="freee_delivery" class="ml-2">
+                    <input type="checkbox" name="is_returnable" id="is_returnable" class="form-check-input">
+                    <label class="ml-2" for="is_returnable">
                         Is Returnable
                     </label>
                 </div>
