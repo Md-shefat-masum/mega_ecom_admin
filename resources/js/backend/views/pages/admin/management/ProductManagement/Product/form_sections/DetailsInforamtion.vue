65c350dcea5e89405fd88893f735c0e528867fe5
Md-shefat-masum <mshefat924@gmail.com>
2024-09-11 01:01:25 +0600

    details information elements

--- resources/js/backend/views/pages/admin/management/ProductManagement/Product/form_sections/DetailsInforamtion.vue
+++ resources/js/backend/views/pages/admin/management/ProductManagement/Product/form_sections/DetailsInforamtion.vue
@@ -7,52 +7,69 @@
             <div class="col-md-12">
                 <label>
                     Video URL
-                    <span class="text-danger">*</span>
-                </label>
-                <input type="text" class="form-control">
-            </div>
-            <div class="col-md-12">
-                <label>
-                    Short Description
-                    <span class="text-danger">*</span>
-                </label>
-                <input type="text" class="form-control">
-            </div>
-            <div class="col-md-12">
-                <label>
-                    Specification
-                    <span class="text-danger">*</span>
                 </label>
                 <input type="text" class="form-control">
             </div>
             <div class="col-md-12">
                 <label>
                     description
-                    <span class="text-danger">*</span>
                 </label>
-                <input type="text" class="form-control">
+                <div class="mt-1 mb-3">
+                    <text-editor :set_value="set_value" :data_store="`product_description`" />
+                    <div class="text_output" v-html="product_description"></div>
+                    <textarea class="d-none" name="product_description"
+                        :value="product_description"></textarea>
+                </div>
             </div>
             <div class="col-md-12">
                 <label>
                     waranty policy
-                    <span class="text-danger">*</span>
                 </label>
-                <input type="text" class="form-control">
+                <div class="mt-1 mb-3">
+                    <text-editor :set_value="set_value" :data_store="`warranty_policy`" />
+                    <div class="text_output" v-html="warranty_policy"></div>
+                    <textarea class="d-none" name="warranty_policy"
+                        :value="warranty_policy"></textarea>
+                </div>
             </div>
             <div class="col-md-12">
                 <label>
                     guraranty policy
-                    <span class="text-danger">*</span>
                 </label>
-                <input type="text" class="form-control">
+                <div class="mt-1 mb-3">
+                    <text-editor :set_value="set_value" :data_store="`guarenty_policy`" />
+                    <div class="text_output" v-html="guarenty_policy"></div>
+                    <textarea class="d-none" name="guarenty_policy"
+                        :value="guarenty_policy"></textarea>
+                </div>
             </div>
-
+            <div class="col-md-12">
+                <short-description />
+            </div>
+            <div class="col-md-12">
+                <specification />
+            </div>
         </div>
     </div>
 </template>
 <script>
+import ShortDescription from './ShortDescription.vue';
+import Specification from './Specification.vue';
 export default {
-
+    components: {
+        ShortDescription,
+        Specification,
+    },
+    data: () => ({
+        product_description: '',
+        warranty_policy: '',
+        guarenty_policy: '',
+    }),
+    methods: {
+        set_value: function (state, value) {
+            this[state] = value;
+        },
+    }
 }
 </script>
 <style lang="">
